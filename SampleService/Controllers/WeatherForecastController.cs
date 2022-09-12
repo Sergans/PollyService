5fbfe88c5c373cc56c8806f7674a07d788bd9b3e
Sergans <gan7er@yandex.ru>
2022-09-12 17:32:29 +0300

    add sampleservice

---
+++ SampleService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public ActionResult  Get()
+        {
+            return Ok();
+        }
+    }
+}
